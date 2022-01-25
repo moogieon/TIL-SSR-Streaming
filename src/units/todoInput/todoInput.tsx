9fd3a2de9dbf96581bbbe69b8223eb44bf294599
moogieon <vzlzydn5007@gmail.com>
2022-01-25 15:05:19 +0900

    email JS test

--- src/units/todoInput/todoInput.tsx
+++ src/units/todoInput/todoInput.tsx
@@ -1,32 +1,23 @@
 import type { NextPage } from "next";
-import { useReducer, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import emailjs from "@emailjs/browser";
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
-
-
 
 const ToDoInput: NextPage = () => {
   const [token, setToken] = useState(false);
+  const form = useRef<any>();
 
-  const submitFormAndShowCaptcha = (e) => {
-    // e.preventDefault();
-    // setToken(true);
-  };
-  const [formState, dispatch] = useState(initialState);
-  function sendEmail(e: any) {
+  const submitFormAndShowCaptcha = (e: any) => {
     e.preventDefault();
+    setToken(true);
+  };
 
-    // const params = { ...formState, "g-recaptcha-response": value };
+  function sendEmail() {
     emailjs
       .sendForm(
         "service_m82vubt",
         "template_9jgwop1",
-        formState,
+        form.current,
         "user_jpanIlQa9WoviTZZHASi2"
       )
       .then(
@@ -39,37 +30,27 @@ const ToDoInput: NextPage = () => {
       );
   }
   return (
-    <article className="my-10">
+    <div className="my-10">
       <div className="text-red-400 text-3xl">
         <input placeholder="what to do?" className="px-4 py-10 my-10"></input>
       </div>
-      <form onSubmit={sendEmail}>
+      <form onSubmit={submitFormAndShowCaptcha} ref={form}>
         <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          onChange={(e) => dispatch()}
-        />
+        <input type="text" name="name" />
         <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          onChange={(e) => dispatch()}
-        />
+        <input type="email" name="email" />
         <label>Message</label>
-        <textarea
-          name="message"
-          onChange={(e) => dispatch({ type: "message", value: e.target.value })}
-        />
+        <textarea name="message" />
         <input type="submit" value="Send" className="border-2" />
+        {token && (
+          <ReCAPTCHA
+            name={"g-recaptcha-response"}
+            sitekey={"6LdAGiUeAAAAAD5bQKxNB-OMFdwWTg85iua5I8wr"}
+            onChange={sendEmail}
+          />
+        )}
       </form>
-      {token && (
-        <ReCAPTCHA
-          sitekey={"6LdAGiUeAAAAAD5bQKxNB-OMFdwWTg85iua5I8wr"}
-          onChange={sendEmail}
-        />
-      )}
-    </article>
+    </div>
   );
 };
 
